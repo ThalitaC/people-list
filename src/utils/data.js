04bcdc60d75e17dfc29ae24dc761ad1d6157b0de
ThalitaC <thalitaceci@gmail.com>
2021-10-03 10:27:40 -0300

    feat: add initial people data

---
+++ src/utils/data.js
@@ -0,0 +1,52 @@
+const people = [
+  {
+		"name": "Caio",
+		"age": 4
+	},
+	{
+		"name": "Ana",
+		"age": 6
+	},
+	{
+		"name": "Carla",
+		"age": 10
+	},
+  {
+		"name": "Denise",
+		"age": 18
+	},
+	{
+		"name": "Debora",
+		"age": 15
+	},
+	{
+		"name": "Rafael",
+		"age": 18
+	},
+  {
+		"name": "Gisele",
+		"age": 44
+	},
+	{
+		"name": "Amelia",
+		"age": 38
+	},
+	{
+		"name": "José",
+		"age": 28
+	},
+  {
+		"name": "Emerson",
+		"age": 98
+	},
+	{
+		"name": "Dalton",
+		"age": 91
+	},
+	{
+		"name": "Maria",
+		"age": 75
+	},
+];
+
+export default people;
